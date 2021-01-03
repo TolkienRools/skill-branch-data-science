ce93aa1d6c21cefc02fd75de59c17c4d59c15e91
vlad <balenkov53@gmail.com>
2021-01-04 01:25:56 +0300

    add

--- task_2_solution.py
+++ task_2_solution.py
@@ -6,3 +6,13 @@ def take_columns(X):
 
 def calculate_target_ratio(X, target_name):
     return round(X[target_name].mean(), 2)
+
+def calculate_data_dtypes(X):
+    return X.dtypes[(X.dtypes == 'float64') | (X.dtypes == 'int') | (X.dtypes == 'object')].count()
+
+def calculate_cheap_apartment(X):
+    return df[df['price_doc'] <= 1000000]['price_doc'].count()
+
+def calculate_squad_in_cheap_apartment(X):
+    return round(df[df['price_doc'] <= 1000000]['full_sq'].mean())
+
