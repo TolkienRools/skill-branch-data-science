e34e9c9e031e5ad45882a0efa34fc066f023df27
vlad <balenkov53@gmail.com>
2021-01-03 12:38:52 +0300

    add gradient

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import copy
+
+def part_deriv(arg, f, index):
+    d_x = 0.0000001
+    tmp = copy.copy(arg)
+    tmp[index] += d_x
+    return round(((f(tmp) - f(arg)) / d_x),2)
+
+
 def derivation(x, f):
     d_x = 0.0000001
     return round(((f(x + d_x) - f(x)) / d_x),2)
-    
+
+
+def gradient(arg, f):
+    grad = []
+
+    for i in range(len(arg)):
+        grad[i] = part_deriv(arg, f, i)
+
+    return grad
