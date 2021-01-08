3d2c1b05b8fccb3eaa79c8fde46e95b5bdee8702
vlad <balenkov53@gmail.com>
2021-01-09 00:44:01 +0300

    fix

--- task_3_solution.py
+++ task_3_solution.py
@@ -5,22 +5,22 @@ def split_data_into_two_samples(X):
     return train_test_split(X, test_size=0.3, random_state=42)
 
 #2
-def prepare_data(X):
-    X.drop(X.dtypes[X.dtypes == 'object'].index, axis=1, inplace=True)
-    X.drop(['id'], axis=1, inplace=True)
-    X.dropna(axis=1, inplace=True)
-    key_feature = X.pop('price_doc')
-    return X, key_feature
+# def prepare_data(X):
+#     X.drop(X.dtypes[X.dtypes == 'object'].index, axis=1, inplace=True)
+#     X.drop(['id'], axis=1, inplace=True)
+#     X.dropna(axis=1, inplace=True)
+#     key_feature = X.pop('price_doc')
+#     return X, key_feature
 
 #3
 def scale_data(X, transformer):
     scaler = transformer
     return scaler.fit_transform(X)
 
-#4
-def prepare_data_for_model(X, transformer):
-    X = scale_data(X, transformer)
-    return prepare_data(X)
+# #4
+# def prepare_data_for_model(X, transformer):
+#     X = scale_data(X, transformer)
+#     return prepare_data(X)
 
 
 
