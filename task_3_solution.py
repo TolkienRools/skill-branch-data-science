272b0fe799c62bdedcd359b9b1b87d46e151cdf1
vlad <balenkov53@gmail.com>
2021-01-15 21:37:24 +0300

    fix prepare data

--- task_3_solution.py
+++ task_3_solution.py
@@ -5,12 +5,13 @@ def split_data_into_two_samples(X):
     return train_test_split(X, test_size=0.3, random_state=42)
 
 #2
-# def prepare_data(X):
-#     X.drop(X.dtypes[X.dtypes == 'object'].index, axis=1, inplace=True)
-#     X.drop(['id'], axis=1, inplace=True)
-#     X.dropna(axis=1, inplace=True)
-#     key_feature = X.pop('price_doc')
-#     return X, key_feature
+def prepare_data(X):
+    X.drop(X.dtypes[X.dtypes == 'object'].index, axis=1, inplace=True)
+    if 'id' in X.columns:
+        X.drop(['id'], axis=1, inplace=True)
+    X.dropna(axis=1, inplace=True)
+    key_feature = X.pop('price_doc')
+    return X, key_feature
 
 #3
 def scale_data(X, transformer):
