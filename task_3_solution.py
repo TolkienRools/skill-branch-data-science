13137ecf45af36a3f5cf554bc0d0a024d2bbc7f3
vlad <balenkov53@gmail.com>
2021-03-23 16:39:44 +0300

    fix

--- task_3_solution.py
+++ task_3_solution.py
@@ -47,9 +47,9 @@ def fit_first_linear_model_2(x_train, y_train):
 #7
 def evaluate_model(linreg, x_pred, y_true):
     y_pred = linreg.predict(x_pred)
-    mse = mean_squared_error(y_test, y_pred)
-    mae = mean_absolute_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+    r2 = r2_score(y_true, y_pred)
     return [round(mse, 2), round(mae, 2), round(r2, 2)]
 
 #8
