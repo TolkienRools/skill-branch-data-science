3203b240c35c54418d8d1b3a291b6468832c9160
vlad <balenkov53@gmail.com>
2021-03-23 11:12:31 +0300

    fix

--- task_3_solution.py
+++ task_3_solution.py
@@ -1,8 +1,8 @@
 from sklearn.model_selection import train_test_split
 
 #1
-def split_data_into_two_samples(X):
-    return train_test_split(X, test_size=0.3, random_state=42)
+def split_data_into_two_samples(x):
+    return train_test_split(x, test_size=0.3, random_state=42)
 
 #2
 def prepare_data(x):
@@ -16,7 +16,7 @@ def prepare_data(x):
 
 #3
 def scale_data(x, transformer):
-    return transformer.fit_transform(X)
+    return transformer.fit_transform(x)
 
 
 
