842752d4e60beb76e4aef87518df0bfbd3c3efa0
vlad <balenkov53@gmail.com>
2021-03-23 11:30:46 +0300

    fix

--- task_3_solution.py
+++ task_3_solution.py
@@ -2,21 +2,19 @@ from sklearn.model_selection import train_test_split
 
 #1
 def split_data_into_two_samples(x):
-    return train_test_split(x, test_size=0.3, random_state=42)
-
+    x_train, x_test = train_test_split(x, test_size=0.3, random_state=42)
+    return [x_train, x_test]
 #2
 def prepare_data(x):
-    key_feature = x['price_doc']
-
+    target_vector = x['price_doc']
     objects = x.select_dtypes(include=['object']).columns
-
-    filter_x = x.drop(columns=['id']).drop(columns=['price_doc']).dropna(axis=1)
-    
-    return [filter_x, key_feature]
+    filtered = x.drop(columns=objects).drop(columns=["id"]).drop(columns=['price_doc']).dropna(axis=1)
+    return [filtered, target_vector]
 
 #3
 def scale_data(x, transformer):
     return transformer.fit_transform(x)
+
 
 
 
